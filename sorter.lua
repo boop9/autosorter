7772dad186e91ae9b7cfbc1dfedda11f9300d962
Armaan Ali Khan <a.khan9064@kgs.edu.pk>
2026-08-02 13:47:15 +0500

    second commit chabnged to rows

--- sorter.lua
+++ sorter.lua
@@ -4,17 +4,46 @@ helpers.getDetails("top")
 
 inventories = peripheral.find("minecraft:barrel")
 
-items = []
 
+partitions = {
+    {name = "exampleitem", inventory = inventories[1], slot = 1}
+}
+
+freeslot = -1
+freeinventory = inventories[1]
 i = 0
-p
+
+function checkIfRowAllocated(inventory,row)
+    for k,v in pairs(partitions) do
+        if v.inventory == inventory then
+            if v.slot == slot then
+                return true
+            end
+        end        
+    end
+end
+
+function setFreeSlotAndInventory()
+-- iterates through each inv and each slot until it finds unallocated slot then sets it as the next free slot
+    for inventory in ipairs(inventories) do
+
+        for row = 1,inventory.size(),9 do
+            details = inventory.getItemDetail(row)
+            if details == nil then
+                if checkIfRowAllocated(inventory,row) == false then 
+                    freerow = row
+                    freeinventory = inventory 
+                    break
+                end
+            end
+        end
+    end
+end
+
+
+
 function partitionItem(itemname)
     -- creates a new partition row for this item
-    found = false
-    repeat 
-        -- iterates through each inventory (i) and each slot until it finds an unallocated slot then allocates 1 slot for that item
-        for slot in ipairs(1,inventories[i].size[]) do
-            
-        end
-    until found == true
+    setFreeSlotAndInventory()
+    table.insert(partitions,{name = itemname, inventory = freeinventory, slot = freeslot})
 end
