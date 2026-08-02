941b781bd5c664c0354aaad400e02d9008301374
Armaan Ali Khan <a.khan9064@kgs.edu.pk>
2026-08-02 20:16:02 +0500

    ddiddy bluuud

--- sorter.lua
+++ sorter.lua
@@ -2,15 +2,14 @@ os.loadAPI("helpers.lua")
 
 helpers.getDetails("top")
 
-inventories = peripheral.find("minecraft:barrel")
+inventories = { peripheral.find("minecraft:barrel") }
+--print(textutils.serialise(inventories))
+-- name, inventory, row
+partitions = {}
 
 
-partitions = {
-    {name = "exampleitem", inventory = inventories[1], row = 1}
-}
-
-freeslot = -1
 freeinventory = inventories[1]
+freerow = 0
 i = 0
 
 function checkIfRowAllocated(inventory,row)
@@ -25,14 +24,18 @@ end
 
 function setFreeRowAndInventory()
 -- iterates through each inv and each slot until it finds unallocated slot then sets it as the next free slot
-    for inventory in ipairs(inventories) do
+    for k,inventory in ipairs(inventories) do
 
-        for row = 1,(inventory.size()//9) do
+        for row = 1,(math.floor(inventory.size() / 9)) do
+            print("number of rows", (math.floor(inventory.size() / 9)))
             details = inventory.getItemDetail((1 * 9 * (row -1) + 1))
             if details == nil then
+                print("found an empty spot")
                 if checkIfRowAllocated(inventory,row) == false then 
                     freerow = row
                     freeinventory = inventory 
+                    print("setting free row as", freerow)
+                    print("setting free inventory as", freeinventory)
                     break
                 end
             end
@@ -43,31 +46,44 @@ end
 
 
 function partitionItem(itemname)
+    print("partitioning item")
     -- creates a new partition row for this item
     setFreeRowAndInventory()
     table.insert(partitions,{name = itemname, inventory = freeinventory, row = freerow})
-
+    print("free row", freerow, "partitioned")
+    --    print(textutils.serialise(partitions))
 end
 
 function getItemPartition(itemname)
+    print("getting item partition")
     for k,v in pairs(partitions) do
         if v.name == itemname then
+            if getFreeSlotInPartition(v) ~= -1 then
                 return v
+            end
         end        
     end  
 end
 
 function getFreeSlotInPartition(partition)
-    rowfirstslot =(1 * 9 * (partition.row -1) + 1)
+    print("getting free slot in partition")
+    rowfirstslot =(1 * 9 * (partition.row) + 1)
     for i = rowfirstslot ,rowfirstslot + 8 do
+        print(i)
         details = partition.inventory.getItemDetail(i)
+        
         if details == nil then
             return i
+        elseif details ~= nil then
+            if details.count < partition.inventory.getItemLimit(i) then
+                 return i
+            end
         else
             return -1
         end
     end
 end
+
 
 diddy =false
 topbarrel = peripheral.wrap("top")
@@ -79,9 +95,12 @@ repeat
         if details ~= nil then
             if getItemPartition(details.name) then
                 partition = getItemPartition(details.name)
-                if getFreeSlotInPartition(partition) then
-                    
+                if getFreeSlotInPartition(partition) ~= -1 then
+                    freeslotinpartition = getFreeSlotInPartition(partition)
+                    topbarrel.pushItems(partition.name,slot,nil,freeslotinpartition)
+                    print("pushing")
                 end
+            else partitionItem(details.name)
             end
         end
     end
